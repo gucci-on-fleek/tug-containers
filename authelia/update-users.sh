feba9c62d870199765fa06573bd0a2db0500aa8e
Max Chernoff <49086429+gucci-on-fleek@users.noreply.github.com>
2025-08-03 06:42:59 +0200

    Fix credential updating script

--- authelia/update-users.sh
+++ authelia/update-users.sh
@@ -5,4 +5,6 @@ root=$(dirname "$0")
 
 cat /home/tug/members/htpasswd | \
     jq --raw-input --slurp --from-file $root/htpasswd-to-authelia.jq > \
-    /home/containers/authelia/conf/users_database.yml
+    ~containers/authelia/conf/users_database.yml
+
+setfacl --modify=group:containers:rw --modify=user:101000:rw ~containers/authelia/conf/users_database.yml
